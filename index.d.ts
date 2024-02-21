81a2875080f67cd3f29512f7f49fd0f643ea0e50
Jordan Harband <ljharb@gmail.com>
2024-02-21 14:00:42 -0800

    [types] fix omission of `v` flag
    yay, type bugs are fun for the whole family

--- index.d.ts
+++ index.d.ts
@@ -4,9 +4,10 @@ type I = 'i';
 type M = 'm';
 type S = 's' | '';
 type U = 'u' | '';
+type V = 'v' | '';
 type Y = 'y' | '';
 
-type Combinations = `${D}${G}${I}${M}${S}${U}${Y}`;
+type Combinations = `${D}${G}${I}${M}${S}${U}${V}${Y}`;
 
 type StringToTuple<S extends string> = S extends `${infer First}${infer Rest}` ? [First, ...StringToTuple<Rest>] : [];
 
